67759c12d8c6b6ef90a1fd3ddfd09a2a5e1c0a65
vitojankovic <vito.jankovic17@gmail.com>
2024-12-04 18:54:02 +0100

    update

--- src/app/math-game/[classNum]/page.js
+++ src/app/math-game/[classNum]/page.js
@@ -7,6 +7,8 @@ export default function MathGame({ params }) {
   const [problems, setProblems] = useState([]);
   const [currentProblem, setCurrentProblem] = useState(null);
   const [showAnswer, setShowAnswer] = useState(false);
+  const [timeLeft, setTimeLeft] = useState(30);
+  const [isTimeUp, setIsTimeUp] = useState(false);
 
   useEffect(() => {
     fetch('/math-formulas.json')
@@ -17,23 +19,41 @@ export default function MathGame({ params }) {
       });
   }, [params.classNum]);
 
+  useEffect(() => {
+    let timerId;
+    if (currentProblem && timeLeft > 0) {
+      timerId = setTimeout(() => {
+        setTimeLeft(prevTime => prevTime - 1);
+      }, 1000);
+    } else if (timeLeft === 0) {
+      setIsTimeUp(true);
+    }
+    return () => clearTimeout(timerId);
+  }, [currentProblem, timeLeft]);
+
   const getRandomProblem = (problemSet) => {
     const randomIndex = Math.floor(Math.random() * problemSet.length);
     setCurrentProblem(problemSet[randomIndex]);
     setShowAnswer(false);
+    setTimeLeft(30);
+    setIsTimeUp(false);
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className={`flex min-h-screen flex-col items-center justify-center p-24 transition-colors duration-300 ${isTimeUp ? 'bg-red-100' : ''}`}>
       <h1 className="text-4xl font-bold mb-8">Matematički šah - {params.classNum}. Razred</h1>
       {currentProblem && (
         <div className="text-center">
           <p className="text-3xl mb-4">{currentProblem.question}</p>
-          {showAnswer && <p className="text-2xl mb-4">Rješenje: {currentProblem.answer}</p>}
-          <div className="flex space-x-4 mt-8">
+          <p className={`text-3xl bold mb-4 ${timeLeft <= 10 ? 'text-red-500' : ''}`}>
+            {timeLeft}
+          </p>
+          {showAnswer && <p className="text-3xl mb-4">Rješenje: {currentProblem.answer}</p>}
+          <div className="flex  justify-center space-x-4 mt-8">
             <button
               onClick={() => setShowAnswer(true)}
               className="bg-black text-white py-2 px-4 rounded-md text-lg hover:bg-gray-800 transition-colors"
+              disabled={isTimeUp}
             >
               Pokaži rješenje
             </button>
@@ -46,9 +66,13 @@ export default function MathGame({ params }) {
           </div>
         </div>
       )}
+      {isTimeUp && (
+        <p className="text-2xl mt-4 text-red-500 font-bold">Vrijeme je isteklo!</p>
+      )}
       <Link href="/select-class" className="mt-8 text-lg underline">
         Odabir razreda
       </Link>
     </main>
   );
 }
+
