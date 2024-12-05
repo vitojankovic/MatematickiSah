d64cfe9d2f22c7f8dd05250b65f52d74ab5376e9
Vito- <99547895+vitojankovic@users.noreply.github.com>
2024-12-05 18:18:21 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-6xl font-bold mb-8">Matematicki šah</h1>
+      <h1 className="text-6xl font-bold mb-8">Matematički šah</h1>
       <Link href="/select-class" className="bg-black text-white py-2 px-4 rounded-md text-lg hover:bg-gray-800 transition-colors">
         Kreni
       </Link>
